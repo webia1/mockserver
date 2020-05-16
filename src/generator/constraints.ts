db50d9ef998df54a096f035300215a193f9a5e85
@webia1 <@webia1>
2020-05-17 00:04:34 +0200

    'ProjectConstraints' is a better name

--- src/generator/constraints.ts
+++ src/generator/constraints.ts
@@ -1,4 +1,4 @@
-export const Projects = [
+export const ProjectConstraints = [
   {
     id: 1,
     name: 'IXE',
--- src/generator/mock-generator.class.ts
+++ src/generator/mock-generator.class.ts
@@ -1,16 +1,16 @@
-import { Projects } from './constraints';
+import { ProjectConstraints } from './constraints';
 import { ProjectType } from './types';
 import * as faker from 'faker/locale/de';
 import { times } from 'lodash';
 export class MockGenerator {
   generateProjects(): Array<ProjectType> {
     const now = new Date();
-    const length = Projects.length;
+    const length = ProjectConstraints.length;
     const projects: Array<ProjectType> = times(length, (n: number) => {
       const past = faker.date.past();
       return {
-        id: Projects[n].id,
-        name: Projects[n].name,
+        id: ProjectConstraints[n].id,
+        name: ProjectConstraints[n].name,
         beschreibung: faker.lorem.words(3),
         createTime: past,
         changeTime: faker.date.between(past, now.toISOString()),
